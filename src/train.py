11b843b5f83207491872a97056cd8f31faa96eea
jperez <03.jon.perez@gmail.com>
2024-11-23 17:53:08 +0100

    🔄 preittier

--- src/train.py
+++ src/train.py
@@ -11,11 +11,8 @@ def compute_gradient(x, y, m_now, b_now):
     return (gradient)
 
 
-def gradient_descent(m_now, b_now, points, L):
-    n = float(len(points))
-    
-    x = points['km_normalized'].values
-    y = points['price_normalized'].values
+def gradient_descent(m_now, b_now, x, y, L):
+    n = float(len(x))
     
     m_gradient = - (2/n) * np.sum(x * (y - (m_now * x + b_now)))
     b_gradient = - (2/n) * np.sum(y - (m_now * x + b_now))
@@ -35,20 +32,19 @@ def train(points, L=0.0001, epsilon=0.0001):
     y_mean = points['price'].mean()
     y_std = points['price'].std()
     
-    points['km_normalized'] = (points['km'] - x_mean) / x_std
-    points['price_normalized'] = (points['price'] - y_mean) / y_std
+    km_normalized = ((points['km'] - x_mean) / x_std).values
+    price_normalized = ((points['price'] - y_mean) / y_std).values
     
-    while (compute_gradient(points['km_normalized'].values, points['price_normalized'].values, m, b) > epsilon):
-        m, b = gradient_descent(m, b, points, L)
+    while (compute_gradient(km_normalized, price_normalized, m, b) > epsilon):
+        m, b = gradient_descent(m, b, km_normalized, price_normalized, L)
 
     m_original = m * (y_std / x_std)
     b_original = (b * y_std) + y_mean - m_original * x_mean
-
-    print(f"Original m: {m_original}, Original b: {b_original}")
 
     return (m_original, b_original)
 
 
 if __name__ == "__main__":
     data = pd.read_csv('lib/data.csv')
-    train(data)
+    m, b = train(data)
+    print(m, b)
