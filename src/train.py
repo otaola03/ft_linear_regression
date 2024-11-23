7d480def76e3e6ca2c53151b5c42bccc2142ae30
jperez <03.jon.perez@gmail.com>
2024-11-23 19:49:28 +0100

    🔄 Plot in progress

--- src/train.py
+++ src/train.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+
 
 
 def compute_gradient(x, y, m_now, b_now):
@@ -39,7 +41,8 @@ def denormalize(x_mean, x_std, y_mean, y_std, m, b):
     return m_original, b_original
 
 
-def train(points, m=0, b=0, L=0.0001, epsilon=0.0001):
+def train(points, m=0, b=0, L=0.0001, epsilon=0.0001 , line_plot=None):
+    print(line_plot)
     x_mean = points['km'].mean()
     x_std = points['km'].std()
     y_mean = points['price'].mean()
@@ -47,8 +50,14 @@ def train(points, m=0, b=0, L=0.0001, epsilon=0.0001):
     
     km_normalized, price_normalized = normalize(x_mean, x_std, y_mean, y_std, points)
     
+    i = 0
     while (compute_gradient(km_normalized, price_normalized, m, b) > epsilon):
         m, b = gradient_descent(m, b, km_normalized, price_normalized, L)
+        if i % 50 == 0:
+            denormalize_m, denormalize_b = denormalize(x_mean, x_std, y_mean, y_std, m, b)
+            line_plot = update_line(points, denormalize_m, denormalize_b, line_plot)
+            print(f'm: {m}, b: {b}')
+        i += 1
 
     m_original, b_original = denormalize(x_mean, x_std, y_mean, y_std, m, b)
 
@@ -61,10 +70,18 @@ def update_data(data, m, b):
     df.loc[0, 'b'] = float(b)
     df.to_csv('lib/line_data.csv', index=False)
 
+
+def on_click(event, data, m, b, line_plot):
+    print('Training...')
+    new_m, new_b = train(data, m, b, L=0.01, epsilon=0.0001, line_plot=line_plot)
+    update_data(data, new_m, new_b)
+    print(new_m, new_b)
+
 def crete_plot(data, m, b):
     x = data.iloc[:, 0]  # Primer columna como eje X
     y = data.iloc[:, 1]  # Segunda columna como eje Y
 
+    # Dibujar los puntos de datos
     plt.scatter(x, y, label='Data points', color='blue')
 
     # Etiquetas y título
@@ -72,19 +89,44 @@ def crete_plot(data, m, b):
     plt.ylabel('price')
     plt.title('Price of a car according to its km')
 
+    # Crear la línea de ajuste (inicialmente con m y b)
+    y_line = m * x + b  # Línea de ajuste
+    line_plot, = plt.plot(x, y_line, label=f'Línea de ajuste: y = {m}x + {b}', color='red', linestyle='--')
+
     # Mostrar la leyenda
     plt.legend()
 
+    ax_button = plt.axes([0.8, 0.05, 0.15, 0.075])  # Posición del botón en el gráfico
+    button = Button(ax_button, 'Train')  # Crear botón
+    button.on_clicked(lambda event: on_click(event, data, m, b, line_plot))
+
     # Mostrar el gráfico
-    plt.show()
+    plt.show()  # No bloquea la ejecución de código
+    # plt.show()
+
+    return line_plot  # Retorna la línea para poder actualizarla
+
+
+def update_line(data, m, b, line_plot):
+    x = data.iloc[:, 0]  # Primer columna como eje X
+
+    # Calcular la nueva línea de ajuste
+    y_line = m * x + b 
+
+    # Actualizar la línea de la gráfica
+    line_plot.set_ydata(y_line)
+
+    # Redibujar el gráfico
+    plt.draw()
+    plt.pause(0.1)
+    return line_plot
+
 
 if __name__ == "__main__":
     data = pd.read_csv('lib/data.csv')
-    actual_m, actual_b = pd.read_csv('lib/line_data.csv').values[0]
+    m = 0
+    b = 0
 
+    line_plot = crete_plot(data, m, b)
+    plt.show()
 
-    new_m, new_b = train(data, actual_m, actual_b)
-
-    update_data(data, new_m, new_b)
-    print(new_m, new_b)
-    crete_plot(data, actual_m, actual_b)
