70e4b504605ab73ca015d07464063fa9cad80cf6
jperez <03.jon.perez@gmail.com>
2024-11-23 15:17:00 +0100

    😃 is working the train

--- src/train.py
+++ src/train.py
@@ -1,20 +1,42 @@
 import pandas as pd
 
-def gradient_descent(m, b, data, L):
-    N = len(data)
+# def gradient_descent(m, b, data, L):
+#     N = len(data)
+#     m_gradient = 0
+#     b_gradient = 0
+#     for index, row in data.iterrows():
+#         x_value = row['km']
+#         y_value = row['price']
+#         
+#         print(f"x_value: {x_value}, y_value: {y_value}")
+#         m_gradient += -(2/N) * x_value * (y_value - ((m * x_value) + b))
+#         b_gradient += -(2/N) * (y_value - ((m * x_value) + b))
+#
+#         # print(f"m_gradient: {m_gradient}, b_gradient: {b_gradient}")
+#
+#     m = m - (L * m_gradient)
+#     b = b - (L * b_gradient)
+#     return (m, b)
+
+def gradient_descent(m_now, b_now, points, L):
     m_gradient = 0
     b_gradient = 0
-    for index, row in data.iterrows():
-        x_value = row['km']
-        y_value = row['price']
-        
-        m_gradient += -(2/N) * x_value * (y_value - ((m * x_value) + b))
-        b_gradient += -(2/N) * (y_value - ((m * x_value) + b))
+    n = float(len(points))
+    for i in range(len(points)):
+        x = points.iloc[i, 0]
+        y = points.iloc[i, 1]
+        print(f"x: {x}, y: {y}")
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    m = m_now - L * m_gradient
+    b = b_now - L * b_gradient
+    return [m, b]
 
-    m = m - (L * m_gradient)
-    b = b - (L * b_gradient)
+
+def train(data, L=0.0001):
+    m = 0
+    b = 0
+    print(data)
+    for i in range(10):
+        m, b = gradient_descent(m, b, data, L)
     return (m, b)
-
-
-def train(data):
-    gradient_descent(0, 0, data, 0.0001)
