7f59644aa5b20f36653ec19dc29124b34f9cdc4e
jperez <03.jon.perez@gmail.com>
2024-11-24 14:44:18 +0100

    🔄 Prettier

--- src/train.py
+++ src/train.py
@@ -3,9 +3,11 @@ import numpy as np
 import pandas as pd
 from matplotlib.widgets import Button
 
+
 def total_error(m, b, x, y):
     total_error = np.sum((y - (m * x + b))**2)
     return total_error / float(len(x))
+
 
 def compute_gradient(x, y, m_now, b_now):
     n = float(len(x))
@@ -15,6 +17,7 @@ def compute_gradient(x, y, m_now, b_now):
     gradient = np.sqrt(m_gradient**2 + b_gradient**2)
 
     return gradient
+
 
 def gradient_descent(m_now, b_now, x, y, L):
     n = float(len(x))
@@ -27,17 +30,20 @@ def gradient_descent(m_now, b_now, x, y, L):
     
     return m, b
 
+
 def normalize(x_mean, x_std, y_mean, y_std, points):
     km_normalized = ((points['km'] - x_mean) / x_std).values
     price_normalized = ((points['price'] - y_mean) / y_std).values
     
     return km_normalized, price_normalized
 
+
 def denormalize(x_mean, x_std, y_mean, y_std, m, b):
     m_original = y_std * m / x_std
     b_original = y_std * b + y_mean - m_original * x_mean
     
     return m_original, b_original
+
 
 def train(points, m=0, b=0, L=0.0001, epsilon=0.00001, line_plot=None, error_plot=None):
     x_mean = points['km'].mean()
@@ -67,6 +73,7 @@ def update_data(data, m, b):
     df.loc[0, 'b'] = float(b)
     df.to_csv('lib/line_data.csv', index=False)
 
+
 #------------------ PLOT ------------------#
 
 def on_click(event, data, m, b, line_plot, error_plot, button):
@@ -76,16 +83,16 @@ def on_click(event, data, m, b, line_plot, error_plot, button):
     print(f"y = {new_m.round(3)}x + {new_b.round(3)}")
     button.set_active(False)
 
+
 def create_plot(data, m, b):
     x = data['km']
     y = data['price']
 
-    plt.ion()  # Activar modo interactivo
+    plt.ion() 
 
-    # Configuración de la figura para la regresión
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-    # Gráfico de la regresión lineal
+    # Linear regression plot
     ax1.scatter(x, y, label='Data points', color='blue')
     ax1.set_xlabel('Km')
     ax1.set_ylabel('Price')
@@ -94,23 +101,24 @@ def create_plot(data, m, b):
     line_plot, = ax1.plot(x, y_line, label=f'y = {m}x + {b}', color='red')
     ax1.legend()
 
-    # Gráfico del error
+    # Error plot
     ax2.set_xlabel('Iterations')
     ax2.set_ylabel('Error')
     ax2.set_title('Error according to the iterations')
     error_plot, = ax2.plot([], [], label='Total Error', color='green')
     ax2.legend()
 
-    # Botón para entrenar
+    # Train button
     ax_button = plt.axes([0.8, 0.05, 0.15, 0.075])
     button = Button(ax_button, 'Train')
     button.on_clicked(lambda event: on_click(event, data, m, b, line_plot, error_plot, button))
 
     plt.subplots_adjust(bottom=0.2)
-    plt.subplots_adjust(hspace=0.5)  # Ajusta el espacio entre los subgráficos
-    plt.show(block=True)  # Esto asegura que la ventana de matplotlib no se cierre automáticamente
+    plt.subplots_adjust(hspace=0.5)
+    plt.show(block=True)
 
     return (line_plot, error_plot)
+
 
 def update_line(data, m, b, line_plot):
     x = data['km']
@@ -124,22 +132,18 @@ def update_line(data, m, b, line_plot):
 
 
 def update_error_plot(iteration, error, error_plot):
-    # Obtener los datos actuales del gráfico
     x_data = np.append(error_plot.get_xdata(), iteration)
     y_data = np.append(error_plot.get_ydata(), error)
     
-    # Actualizar los datos del gráfico
     error_plot.set_xdata(x_data)
     error_plot.set_ydata(y_data)
     error_plot.set_label(f"Total Error = {error:.3f}")
     error_plot.axes.legend()
 
-    # Recalcular y actualizar los límites de los ejes
     ax = error_plot.axes
-    ax.relim()  # Recalcula los límites según los nuevos datos
-    ax.autoscale_view()  # Ajusta la vista de los ejes automáticamente
+    ax.relim()
+    ax.autoscale_view()
 
-    # Redibujar el gráfico
     plt.draw()
     plt.pause(0.1)
 
